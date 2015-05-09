19f44d0f952e85153a6e8a2b5f31d7c453bfb136
Alex Tsilingiris <alex19924@gmail.com>
2015-05-09 23:11:53 +0200

    FManager style if created

--- JCuttleFish/src/main/java/obfuscations/layout/LayoutManager.java
+++ JCuttleFish/src/main/java/obfuscations/layout/LayoutManager.java
@@ -34,25 +34,54 @@ public class LayoutManager {
                 if ( typeDecl.getFields().length != 0 ) {
                     for ( FieldDeclaration fieldDeclaration : typeDecl.getFields() ) {
                         //get(0) translates to get variable declaration fragment.
-                        VariableDeclarationFragment vdf = (VariableDeclarationFragment) fieldDeclaration.fragments().get(0);
+                        VariableDeclarationFragment vdf = ( VariableDeclarationFragment ) fieldDeclaration.fragments().get( 0 );
 
                         originalVarName = vdf.getName();
-                        logger.info( "original var name is : " + originalVarName.getFullyQualifiedName() );
+                        //logger.info( "original var name is : " + originalVarName.getFullyQualifiedName() );
                         //rewriter.remove( fieldDeclaration, null );
                         //ListRewrite listRewrite = rewriter.getListRewrite( typeDecl, TypeDeclaration.BODY_DECLARATIONS_PROPERTY );
                         //listRewrite.insertFirst( fieldDeclaration, null );
+
+                        MethodDeclaration methodDeclaration = typeDecl.getMethods()[ 1 ];
+                        Statement statement = ( Statement ) methodDeclaration.getBody().statements().get( 1 );
+                        Expression expression = null;
+                        if ( statement.getNodeType() == ASTNode.EXPRESSION_STATEMENT ) {
+                            expression = ( ( ExpressionStatement ) statement ).getExpression();
+                            if ( expression.getNodeType() == ASTNode.ASSIGNMENT ) {
+                                Assignment ass = null;
+                                ass = ( Assignment ) expression;
+
+                                if ( ass.getLeftHandSide().getNodeType() == ASTNode.FIELD_ACCESS ) {
+                                    FieldAccess fac = ( FieldAccess ) ass.getLeftHandSide();
+                                    logger.info( "Field access of " + originalVarName.toString() +
+                                            " inside " + methodDeclaration.getName().toString() );
+                                } else if ( ass.getLeftHandSide().getNodeType() == ASTNode.SIMPLE_NAME ) {
+                                    SimpleName sn = ( SimpleName ) ass.getLeftHandSide();
+                                    logger.info( "SimpleName (thus usage) of " + originalVarName.toString() +
+                                            " inside " + methodDeclaration.getName().toString() );
+                                }
+                            }
+                        } else if ( statement.getNodeType() == ASTNode.RETURN_STATEMENT ) {
+                            expression = ( ( ReturnStatement ) statement ).getExpression();
+                            if ( expression.getNodeType() == ASTNode.FIELD_ACCESS ) {
+                                FieldAccess fac = ( FieldAccess ) expression;
+                                logger.info( "Field access, on return, of " + originalVarName.toString() +
+                                        " inside " + methodDeclaration.getName().toString() );
+                            }
+                        } else {
+                            expression = null;
+                        }
+                        //Expression expression = statement
+                        // Assignment assignment = (Assignment)statement.getExpression();
+                        //FieldAccess fieldAccess = (FieldAccess) assignment.getLeftHandSide();
+                        // SimpleName varUsageName = fieldAccess.getName();
+
+                        //logger.info( "usage var name is : " + varUsageName.getFullyQualifiedName() );
                     }
-                    MethodDeclaration methodDeclaration = typeDecl.getMethods()[0];
-                    ExpressionStatement es = (ExpressionStatement)methodDeclaration.getBody().statements().get( 0 );
-                    Assignment assignment = (Assignment)es.getExpression();
-                    FieldAccess fieldAccess = (FieldAccess) assignment.getLeftHandSide();
-                    SimpleName varUsageName = fieldAccess.getName();
-
-
-                    logger.info( "usage var name is : " + varUsageName.getFullyQualifiedName() );
                 }
             }
         }
         return true;
+
     }
 }
